250826b64f7225646832f451e34cfc978a323708
marsherr0610 <marsherr0610@gmail.com>
2022-09-14 09:02:25 +0700

    add logic register with email/password

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from "./../../context/AuthContext";
+
 import './register.css';
 import profile from "../../images/loco-travel.png";
 import account from "../../images/account.png";
@@ -9,6 +11,21 @@ import pass from "../../images/pass.png";
 
 
 function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {user, signUp } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (error){
+      console.log(error);
+    }
+  };
+  console.log(user);
   return (
     <div className="main">
       <div className="sub-main">
@@ -22,24 +39,30 @@ function Register() {
           {/* register form */}
           <div>
             <h1 id='register-title'>Register</h1>
-            <div>
-              <img src={account} alt="pass" className="account" />
-              <input type="text" placeholder="Username" className="name register_input" />
-            </div>
-            <div className='second-input'>
-              <img src={account} alt="pass" className="account" />
-              <input type="text" placeholder="E-mail" className="name register_input" />
-            </div>
-            <div className="second-input">
-              <img src={pass} alt="pass" className="account" />
-              <input type="password" placeholder="Password" className="name register_input" />
-            </div>
-            <div className="second-input">
-              <img src={pass} alt="pass" className="account" />
-              <input type="password" placeholder="Repeat Password" className="name register_input" />
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className='second-input'>
+                <img src={account} alt="pass" className="account" />
+                <input 
+                type="text" 
+                placeholder="E-mail" 
+                className="name register_input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="second-input">
+                <img src={pass} alt="pass" className="account" />
+                <input 
+                type="password" 
+                placeholder="Password" 
+                className="name register_input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </form>
             <div className="register-button">
-              <button id="register-btn" href=""><Link to="/">Sign me up</Link></button>
+              <button id="register-btn" onClick={handleSubmit}>Sign me up</button>
             </div>
           </div>
         </div>
