85e542712c8a93c9f7cf284fd61e2c5acb311fe6
Quan Hoang <marsherr0610@gmail.com>
2023-04-06 09:31:05 +0700

    fix update product item information

--- src/components/Admin/products.js
+++ src/components/Admin/products.js
@@ -1,36 +1,42 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase/firebaseConfig";
-
 import {
   onSnapshot,
   collection,
   addDoc,
-  updateDoc,
   deleteDoc,
   doc,
 } from "firebase/firestore";
-
 import "./products.css";
 import NavBar from './navbar';
 import Footer from '../Footer/footer';
 
 function Products() {
+  const navigate = useNavigate();
+
   const [newName, setNewName] = useState("");
   const [newLocation, setNewLocation] = useState("");
-  const [newImage, setNewImage] = useState("");
   const [newPrice, setNewPrice] = useState(0);
 
   const [products, setProducts] = useState([]);
   const productsCollectionRef = collection(db, "products");
 
   const createProd = async () => {
-    await addDoc(productsCollectionRef, { name: newName, location: newLocation, image: newImage, price: Number(newPrice) });
+    await addDoc(productsCollectionRef, { name: newName, location: newLocation, price: Number(newPrice) });
   };
 
-  const updateProd = async (id, price) => {
-    const prodDoc = doc(db, "products", id);
-    const newFields = { price: price + 10000 };
-    await updateDoc(prodDoc, newFields);
+  function NavigateUpdateProd(id, name, location, price)
+  {
+    navigate("/Admin/Products/updateProduct", 
+    {
+      state:{
+        id: id,
+        name: name,
+        location: location,
+        price: price
+      }
+    })
   };
 
   const deleteProd = async (id) => {
@@ -45,8 +51,7 @@ function Products() {
         ),
       []
     );
-    console.log(products);
-
+  
   return (
   <div className="AdminIndex_bg">
     <NavBar /> 
@@ -59,21 +64,14 @@ function Products() {
         onChange={(event) => {
           setNewName(event.target.value);
         }}
-      />
+      /><br /><br />
       <input
         className="admin_prod_input"
         placeholder="Locate at..."
         onChange={(event) => {
           setNewLocation(event.target.value);
         }}
-      />
-      <input
-        className="admin_prod_input"
-        placeholder="Image..."
-        onChange={(event) => {
-          setNewImage(event.target.value);
-        }}
-      />
+      /><br /><br />
       <input
         className="admin_prod_input"
         type="number"
@@ -81,41 +79,40 @@ function Products() {
         onChange={(event) => {
           setNewPrice(event.target.value);
         }}
-      />
+      /><br /><br />
       <button onClick={createProd} id="create-btn" className="admin_btn"> Create</button>
       <br /><br />
       <table>
         <tr className="table_header_row">
           <th className="column-header">Place</th>
           <th className="column-header">Location</th>
-          <th className="column-header">Image</th>
           <th className="column-header">Ticket Price</th>
           <th className="column-header">Options</th>
         </tr>
-        {products.map((product) => {
+        {products.map((product, item) => {
         return (
-              <tr>
+              <tr key={item}>
                 <td className="place__data">{product.name}</td>
                 <td className="place__data">{product.location}</td>
-                <td className="place__data">{product.image}</td>
                 <td className="place__data">{product.price} VND</td>
                 <td className="place__opts">
                   <button
-                    onClick={() => {
-                      updateProd(product.id, product.price);
-                    }}
                     id="update-btn" className="admin_btn"
+                    onClick={() => {
+                      NavigateUpdateProd(product.id, product.name, product.location, product.price);
+                    }}
                   >
                     Update
                   </button>
+
                   <button
+                    id="delete-btn" className="admin_btn"
                     onClick={() => {
                     deleteProd(product.id);
                     }}
-                    id="delete-btn" className="admin_btn"
                   >
                     Delete
-                  </button>
+                  </button>                  
                 </td>
               </tr>
         );
