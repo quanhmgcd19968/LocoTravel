c3ba288f1bc7fe9e57c77550c199104b20a3ae4d
Quan Hoang <marsherr0610@gmail.com>
2023-04-18 23:38:57 +0700

    adjust the link to product detail page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,11 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from "react";
 import { db } from "./../../firebase/firebaseConfig";
-
-import {
-  onSnapshot,
-  collection
-} from "firebase/firestore";
+import { Link } from "react-router-dom";
+import { onSnapshot, collection } from "firebase/firestore";
 
 import './home.css';
 import Header from "./../Header/header";
@@ -30,14 +27,9 @@ function Home() {
           <h2 class="welcome_message">Get the best prices on online booking and receive tickets before having fun!</h2>
         </div>
         <div id="search-wrapper">
-          <div id="seach-box">
-            <FontAwesomeIcon icon={faSearch} />
-            <span id="seach-box-placeholder">
-              <input type="text" id='search-box-input' placeholder='Where you wanna go in Danang?'/>
-            </span>
-          </div>
+          <p id='introduction-homepage'>Proud to be a platform offers curated travel experiences to offbeat destinations, connect travelers with local hosts who are passionate about showcasing their culture, traditions, and hidden gems.</p>
           <button id="search-btn">
-            SEARCH
+            LET'S GO EXPLORE !
           </button>
         </div>
       </section>
@@ -46,25 +38,29 @@ function Home() {
         <div id="places-list">
         {products.map((product) => {
         return (
+        <div>
           <div className="place_item">
             <a href="#" className="link_place">
               <div className="place_img">
-                <img src="https://www.creativefabrica.com/wp-content/uploads/2019/02/Place-Icon-by-Kanggraphic.jpg" />
+                <Link to={`/products/${product.id}`}>
+                  <img src="https://www.creativefabrica.com/wp-content/uploads/2019/02/Place-Icon-by-Kanggraphic.jpg" />
+                </Link>
               </div>
               <div className="place_name">
-                <p>{product.name}</p>
+                <Link to={`/products/${product.id}`}>{product.name}</Link>
               </div>
               <div className="place_locate">
                 <br />
                 <FontAwesomeIcon icon={faLocationDot} />
-                <span> {product.location}</span>
+                <Link to={`/products/${product.id}`}><span> {product.location}</span></Link>
               </div>
               <div className="place_price">
                 <p>{product.price} VND</p>
               </div>
             </a>
           </div>
-          );
+        </div>
+        );
         })}
         </div>
       </div>
